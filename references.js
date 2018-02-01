640cb631d8e9689b8771fca7a8b05bb9a80a9b48
jobeau <jordboudreau@gmail.com>
2018-02-01 03:07:31 +0000

    refactor models into separate files

--- references.js
+++ references.js
@@ -2,57 +2,18 @@ var mongoose = require("mongoose");
 
 mongoose.connect("mongodb://localhost/blog_demo_2");
 
-//POST - title, content
-var postSchema = new mongoose.Schema({
-    title: String,
-    content: String
-});
+var Post = require("./models/post");
+var User = require("./models/user");
 
-var Post = mongoose.model("Post", postSchema);
 
-// USER - email, name
-var userSchema = new mongoose.Schema({
-    email: String,
-    name: String,
-    posts: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Post"
-        }
-    ]
-});
-
-var User = mongoose.model("User", userSchema);
-
-// Post.create({
-//     title: "How to cook the bset burger pt. 3",
-//     content: "asdfdsfgafh"
-// }, function(err, post){
-//     User.findOne({email: "bob@gmail.com"}, function(err, foundUser){
-//         if(err){
-//             console.log(err);
-//         }else{
-//             foundUser.posts.push(post._id);
-//             foundUser.save(function(err, data){
-//                 if(err){
-//                     console.log(err);
-//                 }else{
-//                     console.log(data);
-//                 }
-//             })
-//         }
-//     });
-// });
-
-// User.create({
-//     email: "bob@gmail.com",
-//     name: "Bob Belcher"
-// });
-
-User.findOne({email: "bob@gmail.com"}).populate("posts").exec(function(err, user){
+Post.create({
+    title: "hello",
+    content: "yo"
+}, function(err, post){
     if(err){
-        console.log(err);
+        console.log(err)
     }else{
-        console.log(user);
+        
+        console.log(post);
     }
-});
+})
